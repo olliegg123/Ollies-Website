e9db63366789076456c65267d1c96dee1ac0a484
Oliver Gerrard (ogerrard) <olliegg123@gmail.com>
2021-08-08 18:42:43 +0100

    Update buttons.tsx

--- src/components/buttons.tsx
+++ src/components/buttons.tsx
@@ -6,8 +6,8 @@ export const AllButtons = () => {
     const [show, setShow] = useState(true)
     return (
         <div onTouchStart="" className="flex-container">
-        <a className="btn flex-item" target="_blank" href="http://olliegg.co.uk/games/cards.html"><span className="span2">Liver Let Die</span></a>
-        <a className="btn flex-item" target="_blank" href="http://olliegg.co.uk/games/GoS2.html"><span className="span2">Game of Speeches</span></a>
+        <a className="btn flex-item" target="_blank" href="http://olliegg123.github.io/games/cards.html"><span className="span2">Liver Let Die</span></a>
+        <a className="btn flex-item" target="_blank" href="http://olliegg123.github.io/games/GoS2.html"><span className="span2">Game of Speeches</span></a>
         <a className="btn flex-item" target="_blank"href="https://apps.apple.com/gb/app/this-is-another-card-game/id1541317739"><span className="bigSpan">App Store Link<br /> This Is Another Card Game</span></a>
         </div>
     )
